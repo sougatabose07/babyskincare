d67e92e8ac900f2dec3eed5a3cc443d6ce2ff276
Sougata Bose <sougata@nottinghillmedia.com>
2026-07-13 09:42:24 +0530

    logout fixed

--- app/Http/Controllers/AdminAuthController.php
+++ app/Http/Controllers/AdminAuthController.php
@@ -57,6 +57,6 @@ class AdminAuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect()->route('login');
+        return redirect()->route('admin.login');
     }
 }
